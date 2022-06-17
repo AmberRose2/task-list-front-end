5555ea2b15926abc4e539682a7f6c21004658471
Amber <ambertanaka@yahoo.com>
2022-06-17 14:04:20 -0700

    coworking day 1, tasks appear but button is broken

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+import { useState } from 'react';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Mow the lawn',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Cook Pasta',
+      isComplete: true,
+    },
+  ]);
+  const flipComplete = (id) => {
+    console.log('ARE WE HERE?!?!!?!');
+    for (const task of tasks) {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+    }
+    const newTasks = [...tasks];
+    setTasks(newTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={tasks} taskFinished={flipComplete} />}</div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = (props) => {
+  // const [complete, setComplete] = useState(props.isComplete);
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
 
+  const completeTask = () => {
+    props.taskFinished(props.id);
+  };
   return (
-    <li className="tasks__item">
+    <li className="tasks__item" key={props.id}>
+      {props.id}
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={completeTask}
       >
-        {title}
+        {props.title}
       </button>
       <button className="tasks__item__remove button">x</button>
     </li>
@@ -24,6 +27,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  taskFinished: PropTypes.func.isRequired,
 };
 
 export default Task;
