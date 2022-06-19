a5a4ee765936a42a5a6bf27a0d67fe26b7a9ed73
Sreymompech <sanchhen2013@gmail.com>
2022-06-18 21:14:26 -0700

    Wave2 code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react/cjs/react.production.min';
+import Task from './components/Task.js';
 
-const TASKS = [
+{
+  /*const TASKS = [
   {
     id: 1,
     title: 'Mow the lawn',
@@ -13,16 +16,41 @@ const TASKS = [
     title: 'Cook Pasta',
     isComplete: true,
   },
-];
+]; */
+}
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Mow the lawn',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Cook Pasta',
+      isComplete: true,
+    },
+  ]);
+  // const flipisComplete = (id) => {
+  //   const updateTasks = tasks.map((task) => {
+  //     if (task.id === id) {
+  //       task.isComplete = !task.isComplete;
+  //     }
+  //     return task;
+  //   });
+  //   settasks(updateTasks);
+  //};
+  // isCompleteCallBack={flipisComplete}
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          <TaskList tasks={tasks} />
+        </div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = (props) => {
+  //const [complete, setComplete] = useState(props.isComplete);
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
+  // const toggleComplete = () => {
+  //   props.isCompleteCallBack(id);
+  // };
+  // onClick={toggleComplete}
 
   return (
-    <li className="tasks__item">
-      <button
-        className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
-      >
-        {title}
+    <li className="tasks__item" key={props.id}>
+      <button className={`tasks__item__toggle ${buttonClass}`}>
+        {props.title}
       </button>
       <button className="tasks__item__remove button">x</button>
     </li>
